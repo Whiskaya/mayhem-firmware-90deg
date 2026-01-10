c79caae3568c8d2bf75a6e802c3572e0aeba4c92
Matias Vizcaya <64118649+Whiskaya@users.noreply.github.com>
2026-01-10 13:53:17 -0300

    Modify LCD orientation settings for 90° rotation CW (alternative LCD)
    Only changes 122-126 for using alternative LCD screen that is rotated.

--- firmware/common/lcd_ili9341.cpp
+++ firmware/common/lcd_ili9341.cpp
@@ -118,12 +118,11 @@ void lcd_init() {
 
     io.lcd_data_write_command_and_data(0xC5, {0x00, 0x66, 0x80});  // VCOM
 
-    io.lcd_data_write_command_and_data(0x36, {
-                                                 (1 << 7) |  // MY=1
-                                                 (0 << 6) |  // MX=0
-                                                 (0 << 5) |  // MV=0
-                                                 (1 << 4) |  // ML=1: reverse vertical refresh to simplify scrolling logic
-                                                 (1 << 3)    // BGR=1: For Kingtech LCD, BGR filter.
+    // 90° CW: MX|MV|BGR = 0x68
+    io.lcd_data_write_command_and_data(0x36, { 
+                                                (1 << 6) |  // MX=1
+                                                (1 << 5) |  // MV=1 (swapea rows/columns)
+                                                (1 << 3)    // BGR=1
                                              });
 
     // io.lcd_data_write_command_and_data(0x36, {0x48});
